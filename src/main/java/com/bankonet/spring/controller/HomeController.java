740b736291dd9844a5f3d520f926957cb9f66ffa
kells <kelian.bousquet@laposte.net>
2019-01-14 10:36:50 +0100

    test build fail jenkins

--- src/main/java/com/bankonet/spring/controller/HomeController.java
+++ src/main/java/com/bankonet/spring/controller/HomeController.java
@@ -1,16 +1,6 @@
 package com.bankonet.spring.controller;
 
-import com.bankonet.spring.model.Client;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+public class HomeController {
 
-import java.util.List;
-
-public class HomeController
-{
-    public List<Client> getAllClients() {
-
-        return (List<Client>) clientRepository.findAll();
-    }
+    return interface;
 }
